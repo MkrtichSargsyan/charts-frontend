5db0a7007c6117fd515cce35b0063f97fe83dc1f
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-08-08 16:03:26 +0400

    Fetch the covid data from api and show in a chart

--- src/Graph.js
+++ src/Graph.js
@@ -15,60 +15,58 @@ function Graph() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    getData();
-  });
-
-  const getData = () => {
-    var axios = require('axios');
-    var data = '';
-
     var config = {
       method: 'get',
+      mode: 'no-cors',
       url: 'http://localhost:3000/',
       headers: {},
-      data: data,
     };
 
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
+    const fetchData = async () => {
+      try {
+        const d = await axios(config).then((response) => response.data);
+        setData(d);
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
 
+    fetchData();
+  }, []);
+
+  console.log(data);
   return (
-    <LineChart
-      // stroke={(d) => (d > 50 ? 'red' : 'green')}
-      width={1200}
-      height={500}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="deaths"
-        stroke="red"
-        activeDot={{ r: 8 }}
-      />
-      <Line
-        type="monotone"
-        dataKey="recovered"
-        stroke="blue"
-        activeDot={{ r: 8 }}
-      />
-    </LineChart>
+    data && (
+      <LineChart
+        width={1200}
+        height={500}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="deaths"
+          stroke="red"
+          activeDot={{ r: 8 }}
+        />
+        <Line
+          type="monotone"
+          dataKey="recovered"
+          stroke="blue"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+    )
   );
 }
 
