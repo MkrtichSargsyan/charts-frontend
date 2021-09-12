1de07f44458a3370f5c00e4cfda219f91390c83b
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-09-12 15:50:33 +0400

    Add pie chart and refresh page every 10 seconds

--- src/Graph.js
+++ src/Graph.js
@@ -9,10 +9,26 @@ import {
   XAxis,
   YAxis,
 } from 'recharts';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 import axios from 'axios';
 
 function Graph() {
   const [data, setData] = useState([]);
+  const data01 = [];
+  const data02 = [];
+
+  console.log('1');
+
+  for (const key in data) {
+    data01.push({
+      name: data[key].month,
+      value: data[key].recovered,
+    });
+    data02.push({
+      name: data[key].month,
+      value: data[key].deaths,
+    });
+  }
 
   useEffect(() => {
     var config = {
@@ -31,41 +47,65 @@ function Graph() {
       }
     };
 
-    fetchData();
+    setInterval(() => {
+      fetchData();
+    }, 10000);
   }, []);
 
   return (
     data && (
-      <LineChart
-        className="chart"
-        width={1200}
-        height={500}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="month" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="deaths"
-          stroke="red"
-          activeDot={{ r: 8 }}
-        />
-        <Line
-          type="monotone"
-          dataKey="recovered"
-          stroke="blue"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
+      <div className="flex">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data02}
+            dataKey="value"
+            cx="50%"
+            cy="50%"
+            outerRadius={60}
+            fill="#8884d8"
+          />
+          <Pie
+            data={data01}
+            dataKey="value"
+            cx="50%"
+            cy="50%"
+            innerRadius={70}
+            outerRadius={90}
+            fill="#82ca9d"
+            label
+          />
+        </PieChart>
+        <LineChart
+          className="chart"
+          width={900}
+          height={500}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="deaths"
+            stroke="red"
+            activeDot={{ r: 8 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="recovered"
+            stroke="blue"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </div>
     )
   );
 }
